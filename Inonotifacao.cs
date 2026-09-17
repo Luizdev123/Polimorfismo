444c565fb025438d3611b2cb1ea396577fcb909b
Luizdev123 <luiz.crispim@edu.senai.br>
2026-09-17 11:54:33 -0300

    Inonotificacao

---
+++ Inonotifacao.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polimorfismo
+{
+    
+    internal interface INotificacao
+    {
+        void Enviar();
+    }
+
+    
+    internal class NotificacaoEmail : INotificacao
+    {
+        public void Enviar()
+        {
+            Console.WriteLine("Enviando notificação por email...");
+        }
+
+        
+        public void Enviar(string anexo)
+        {
+            Console.WriteLine($"Enviando email com anexo: {anexo}");
+        }
+    }
+
+    
+    internal class NotificacaoSms : INotificacao
+    {
+        public void Enviar()
+        {
+            Console.WriteLine("Enviando notificação por SMS...");
+        }
+    }
+
+    
+    internal class NotificacaoPush : INotificacao
+    {
+        public void Enviar()
+        {
+            Console.WriteLine("Enviando notificação push para o app...");
+        }
+    }
+}

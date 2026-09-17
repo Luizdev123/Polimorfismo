ab40f4d4865d6db633ea8d7778ed48004158cf7c
Luizdev123 <luiz.crispim@edu.senai.br>
2026-09-17 09:11:59 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,20 @@
+﻿namespace Abstracao
+{
+    internal class Geometria
+    {
+        public double CalcularArea(double lado)
+        {
+            return lado * lado;
+        }
+
+        public double CalcularArea(double baseRetangulo, double altura)
+        {
+            return baseRetangulo * altura;
+        }
+
+        public double CalcularArea(double raio, bool isCirculo)
+        {
+            return Math.PI * raio * raio;
+        }
+    }
+}
